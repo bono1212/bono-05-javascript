3feced6cb33fdbe113b61ffa69181ceb2a606416
bono1212 <uyanga1212g@gmail.com>
2020-09-23 12:29:54 +0900

    gg

--- js/18.ajax.js
+++ js/18.ajax.js
@@ -1,28 +1,36 @@
-var scores = {
-	students: [
-		{
-			name:"dong",
-			kor: 95,
-			eng: 80,
-			math: 92
-		},
-		{
-			name:"sung",
-			kor: 90,
-			eng: 75,
-			math: 90
-		},
-		{
-			name:"man",
-			kor: 65,
-			eng: 85,
-			math: 88
-		},
-	],
-	total: 3
+function onAjax() {
+	$.ajax({
+		type: 'get',
+		dataType: 'json',
+		url: '../json/score.json',
+		success: function (r){
+			console.log(r);
+		}
+	});
 }
 
-var scoresJSON = JSON.stringify(scores);
+function onAjaxTeacher() {
+	//$.get(url, sendData, callback);
+	$.get('http://192.168.0.14:5500/json/score.json', function (r){
+		console.log(r);
+	});
+}
 
-console.log(scores);
-console.log(scoresJSON);
+
+function onWeather() {
+	var url = 'http://api.openweathermap.org/data/2.5/weather';
+	var data = {
+		appid: 'b059e459e8e4a4f9eee7a5505a60b714',
+		id: '1835848',
+		units: 'metric'
+	};
+	var success= function(r) {
+		console.log(r);
+	}
+	$.get(url, data, success);
+}
+
+
+$("#btnMY").click(onAjax);
+$("#btnTeacher").click(onAjaxTeacher);
+$("#btnWeather").click(onWeather);
