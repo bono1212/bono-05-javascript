892383f1d19d945f47c9f865939b9e06492c23f5
bono1212 <uyanga1212g@gmail.com>
2020-10-07 12:15:06 +0900

    gg

--- js/bono-slide.js
+++ js/bono-slide.js
@@ -1,3 +1,21 @@
-var Slide = function() {
+var Slide = function(arg) {
+	this.slides = arg.slide;
+	this.container = arg.container;
+	this.wrapper = $('<div class="bono-wrapper"></div>').appendTo($(this.container));
+	this.direction = arg.direction || 'hoti';
+	this.autostart = arg.autostart || false;
+	this.gapSpeed = arg.gapSpeed || 3000;
+	this.aniSpeed = arg.aniSpeed || 500;
+	this.init();
+	return this;
+}
 
+Slide.prototype.init = function(){
+	var html = '';
+	for(var i=0; i<this.slides.length; i++) {
+	html = '<div class="bono-slide">';
+	html += '<img src="'+this.slides[i]+'" class="img">';
+	html += '</div>';
+	$(this.wrapper).append(html);
+	}
 }
